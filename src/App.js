addfe20894d23d87517fb53738e28aead8be2b4f
Jakub Staniek <jakub.staniek@icloud.com>
2022-05-21 22:21:09 +0200

    Adding reducers instead of useState

--- src/App.js
+++ src/App.js
@@ -4,24 +4,27 @@ import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import { GithubProvider } from "./context/github/GithubContext";
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar />
-        <main className="container mx-auto px-3 pb-12">
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/about" element={<About />} />
-            <Route exact path="/notfound" element={<NotFound />} />
-            <Route exact path="/*" element={<NotFound />} />{" "}
-            {/* Something (sub-page) that doesn't exist*/}
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <GithubProvider>
+      <Router>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar />
+          <main className="container mx-auto px-3 pb-12">
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/about" element={<About />} />
+              <Route exact path="/notfound" element={<NotFound />} />
+              <Route exact path="/*" element={<NotFound />} />{" "}
+              {/* Something (sub-page) that doesn't exist*/}
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   );
 }
 
