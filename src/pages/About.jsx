791cb07734f74bd9d8ccb7234c6f01e03077cfb3
Jakub Staniek <jakub.staniek@icloud.com>
2022-05-26 11:35:28 +0200

    Changing About message

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,10 +4,10 @@ const About = () => {
       <h1 className="text-6xl mb-4">Github Finder</h1>
       <p className="mb-4 text-2xl font-light">
         A React app to search GitHub profiles and see profile details. This
-        project is part of the
+        project was made during
         <a href="https://www.udemy.com/course/modern-react-front-to-back/">
           {" "}
-          React Front To Back
+          <strong>React Front To Back</strong>
         </a>{" "}
         Udemy course by
         <strong>
